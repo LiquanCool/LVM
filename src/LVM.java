d200e650a45f19ef851cd14eb060f4a584ca9f24
BT_4N2_31 <llei0111@bths.edu>
2023-03-20 12:29:57 -0400

    Finished Design Document

---
+++ src/LVM.java
@@ -0,0 +1,11 @@
+import java.util.UUID;
+public class LVM {
+    private String name;
+    private UUID ID;
+    public LVM(String n)
+    {
+        name = n;
+        ID = UUID.randomUUID();
+    }
+
+}
