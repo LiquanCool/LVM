7a32fc71c7e8d5a13387a13ff0ec632a891c0d15
BT_4N2_31 <llei0111@bths.edu>
2023-03-23 12:29:23 -0400

    Did 4/8 methods only tested two

--- src/Command.java
+++ src/Command.java
@@ -1,2 +1,83 @@
+import java.util.ArrayList;
+
 public class Command {
+    private ArrayList<HardDrive> driveList = new ArrayList<HardDrive>();
+    private ArrayList<PhysicalVolume> PVList = new ArrayList<PhysicalVolume>();
+    public Command()
+    {
+    }
+    public void installDrive(String name, int size)
+    {
+        boolean installed = false;
+        for (int i =0;i<driveList.size();i++)
+        {
+            if (driveList.get(i).getName().equals(name))
+            {
+                installed = true;
+            }
+        }
+        if (installed)
+        {
+            System.out.println("Error, drive already installed");
+        }
+        else
+        {
+            HardDrive h = new HardDrive(name,size);
+            driveList.add(h);
+            System.out.println("Drive "+ name + " installed");
+        }
+    }
+    public void listDrives()
+    {
+        for (int i =0;i<driveList.size();i++)
+        {
+            HardDrive current = driveList.get(i);
+            System.out.println(current.getName()+" ["+current.getSize()+"G]["+current.getID()+"]");
+        }
+    }
+    public void createPV(String PVname, String driveName)
+    {
+        boolean installed = false;
+        for (int i =0;i<PVList.size();i++)
+        {
+            if (PVList.get(i).getName().equals(PVname))
+            {
+                installed = true;
+                System.out.println("Error, PV already installed");
+            }
+            if(PVList.get(i).getAssociatedHD().getName().equals(driveName))
+            {
+                installed = true;
+                System.out.println("Error, drive already associated");
+            }
+        }
+        boolean notExists = true;
+        HardDrive theDrive = null;
+        for (int i =0;i<driveList.size();i++)
+        {
+            if (driveList.get(i).getName().equals(driveName))
+            {
+                notExists = false;
+                theDrive = driveList.get(i);
+            }
+        }
+        if (notExists)
+        {
+            System.out.println("Error, drive does not exist");
+        }
+        else if(!installed)
+        {
+            PhysicalVolume p = new PhysicalVolume(PVname,theDrive);
+            PVList.add(p);
+            System.out.println(PVname + " created");
+        }
+    }
+    public void listPVs()
+    {
+        for (int i =0;i<PVList.size();i++)
+        {
+            PhysicalVolume current = PVList.get(i);
+            System.out.println(current.getName()+": ["+current.getAssociatedHD().getSize()+"G] ["+current.getID()+"]");
+        }
+    }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
-
+        Scanner n = new Scanner(System.in);
+        String input = "";
+        Command main = new Command();
+        System.out.println("Welcome to the LVM system");
+        while(input!="exit")
+        {
+            System.out.print("cmd#: ");
+            input = n.nextLine();
+            String[] splitInput = input.split(" ");
+            //for (int i =0;i<splitInput.length;i++)
+            //{
+            //    System.out.println(splitInput[i]);
+            //}
+            if (splitInput[0].equals("install-drive"))
+            {
+                System.out.println("working");
+                main.installDrive(splitInput[1],Integer.parseInt(splitInput[2].substring(0,splitInput[2].length()-1)));
+            }
+            else if(splitInput[0].equals("List-drives"))
+            {
+                main.listDrives();
+            }
+            else if(splitInput[0].equals("pvcreate"))
+            {
+                main.createPV(splitInput[1],splitInput[2]);
+            }
+            else if(splitInput[0].equals("pvlist"))
+            {
+                main.listPVs();
+            }
+        }
     }
 }
